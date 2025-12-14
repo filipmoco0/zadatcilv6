0ee24047e632eeba9eb261f4f8ae37f238a633f4
filipmoco0 <113348795+filipmoco0@users.noreply.github.com>
2025-12-14 23:18:48 +0100

    Refactor Automobil struct and implement file output
    Refactor Automobil struct and add file writing functionality.

--- zadatak1.cs
+++ zadatak1.cs
@@ -1,66 +1,31 @@
 using System;
-using System.Collections.Generic;
-
-struct Automobil
-{
-    public string marka;
-    public int godinaProizvodnje;
-    public int obujamMotora;     // u cm3
-    public int kilometri;
-
-    // Funkcija za ažuriranje kilometara
-    public void AzurirajKilometre(int noviKm)
-    {
-        kilometri = noviKm;
-    }
-
-    // Funkcija za usporedbu obujma motora
-    public void UsporediObujam(Automobil drugi)
-    {
-        if (obujamMotora == drugi.obujamMotora)
-            Console.WriteLine("Jednak obujam motora.");
-        else if (obujamMotora > drugi.obujamMotora)
-            Console.WriteLine("Prvi automobil ima veći obujam.");
-        else
-            Console.WriteLine("Drugi automobil ima veći obujam.");
-    }
-
-    // Funkcija koja vraća starost auta
-    public int Starost()
-    {
-        return DateTime.Now.Year - godinaProizvodnje;
-    }
-}
+using System.IO;
 
 class Program
 {
     static void Main()
     {
-        // A) KREIRANJE AUTOMOBILA
-        Automobil a1 = new Automobil { marka = "Audi", godinaProizvodnje = 2015, obujamMotora = 2000, kilometri = 150000 };
-        Automobil a2 = new Automobil { marka = "BMW", godinaProizvodnje = 2010, obujamMotora = 2500, kilometri = 220000 };
-        Automobil a3 = new Automobil { marka = "Opel", godinaProizvodnje = 2018, obujamMotora = 1600, kilometri = 90000 };
-
-        // Usporedba prva dva
-        a1.UsporediObujam(a2);
-
-        // Ažuriranje trećeg i usporedba s prvim
-        a3.AzurirajKilometre(95000);
-        a3.UsporediObujam(a1);
-
-        // Spremanje u listu
-        List<Automobil> automobili = new List<Automobil> { a1, a2, a3 };
-
-
-        // B) ISPIS AUTA STARijih OD 6 GODINA I OB. MOTORA < 2200 cm3
-        Console.WriteLine("\nAutomobili stariji od 6 godina i s obujmom motora < 2200 cm3:");
-
-        foreach (var auto in automobili)
+        string path = "Zad1.txt";
+        using (StreamWriter sw = new StreamWriter(path))
         {
-            if (auto.Starost() > 6 && auto.obujamMotora < 2200)
+            for (int i = 8; i <= 142; i++)
             {
-                Console.WriteLine($"{auto.marka} - Godina: {auto.godinaProizvodnje}, Obujam: {auto.obujamMotora} cm3, Starost: {auto.Starost()} god");
+                if (i % 7 == 0 && !SveZnamenkeJednake(i))
+                {
+                    sw.WriteLine(i);
+                }
             }
         }
+        Console.WriteLine("Zapisano u Zad1.txt.");
+    }
+
+    static bool SveZnamenkeJednake(int broj)
+    {
+        string s = broj.ToString();
+        foreach (char c in s)
+        {
+            if (c != s[0]) return false;
+        }
+        return true;
     }
 }
