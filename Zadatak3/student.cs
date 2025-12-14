f28d3ee6d635a3f6a43d05f871b21c0f273c731c
filipmoco0 <113348795+filipmoco0@users.noreply.github.com>
2025-12-14 23:31:04 +0100

    Add Student class with properties

---
+++ Zadatak3/student.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV6_Zadatak3
+{
+    internal class Student
+    {
+         public string Ime;
+         public string Prezime;
+         public string Jmbag;
+         public int GodinaRodenja;
+         public List<Ocjena> Ocjene;
+    }
+}
