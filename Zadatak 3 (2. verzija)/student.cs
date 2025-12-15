4cae68f0f189e13d6e95471033279c5c4dde57ea
filipmoco0 <113348795+filipmoco0@users.noreply.github.com>
2025-12-15 09:36:04 +0100

    Add Student class with properties and list of grades

---
+++ Zadatak 3 (2. verzija)/student.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+class Student
+{
+    public string Ime { get; set; }
+    public string Prezime { get; set; }
+    public string JMBAG { get; set; }
+    public List<Ocjena> Ocjene { get; set; }
+}
